7cca980c3d5e8693c47579a2ea9d4243afcadf21
Jack Serrino <Jserrino@gmail.com>
2016-02-02 21:15:24 -0500

    more

--- predictit.py
+++ predictit.py
@@ -6,9 +6,10 @@ URL_BASE = "https://www.predictit.org/Home/GetContractListAjax?marketId="
 
 
 class Market:
-    def __init__(self):
+    def __init__(self, market_id):
         self.contestants = []
         self.name = ""
+        self.id = market_id
 
     def add_contestant(self, name, buy_yes, sell_yes):
         self.contestants.append((name, buy_yes, sell_yes))
@@ -28,7 +29,7 @@ def get_title(soup):
 
 def parse_market(response):
     try:
-        market = Market()
+        market = Market(response.url[-4:])
         soup = BeautifulSoup(response.text, "html.parser")
         market.name = get_title(soup)
         for match in soup.find('table', id="contractListTable").find('tbody').find_all('tr'):
@@ -55,16 +56,16 @@ def get_data():
     result = ""
     market_ids = None
     with open('./markets.txt', 'r') as f:
-        market_ids = f.read().strip().split('\n')
+        market_ids = list(set(f.read().strip().split('\n')))
 
     responses = grequests.map([grequests.get(URL_BASE + market_id) for market_id in market_ids])
 
     markets = [parse_market(response) for response in responses if response.status_code == 200]
 
-    result += ("        market name | bye | bne | byt | bnt |\n")
-    market_results = sorted([(market.name,) + market.edges() for market in markets if market is not None], key=lambda x: -x[2])
+    result += ("        market name |   id | bye | bne | byt | bnt |\n")
+    market_results = sorted([(market.name, market.id) + market.edges() for market in markets if market is not None], key=lambda x: -x[3])
     for market in market_results:
-        result += ("{: >19s} | {: >3d} | {: >3d} | {: >3d} | {: >3d} |\n".format(*market))
+        result += ("{: >19s} | {: >4s} | {: >3d} | {: >3d} | {: >3d} | {: >3d} |\n".format(*market))
     return result
 
 
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import BaseHTTPServer
 import predictit
 import time
-
+from urlparse import urlparse
 
 HOST_NAME = '0.0.0.0'  # !!!REMEMBER TO CHANGE THIS!!!
 PORT_NUMBER = 8000  # Maybe set this to 9000.
@@ -10,7 +10,28 @@ PORT_NUMBER = 8000  # Maybe set this to 9000.
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(s):
         """Respond to a GET request."""
-        if s.path != "/":
+        query = urlparse(s.path).query
+        query_components = dict(qc.split("=") for qc in query.split("&") if query != '')
+        if urlparse(s.path).path == "/add":
+            with open('markets.txt','a') as f:
+                f.write(query_components['id'] + '\n')
+            s.send_response(200)
+            s.send_header("Content-type", "text/plain")
+            s.end_headers()
+            s.wfile.write("Ok, added " + query_components['id'] + "\n")
+            return
+        if urlparse(s.path).path == "/remove":
+            with open('./markets.txt', 'r') as f:
+                market_ids = set(f.read().strip().split('\n'))
+            market_ids.discard(query_components['id'])
+            with open('./markets.txt', 'w') as f:
+                f.write('\n'.join(list(market_ids)) + '\n')
+            s.send_response(200)
+            s.send_header("Content-type", "text/plain")
+            s.end_headers()
+            s.wfile.write("Ok, removed " + query_components['id'] + "\n")
+            return
+        if urlparse(s.path).path != "/":
             s.send_response(404)
             s.end_headers()
             return
